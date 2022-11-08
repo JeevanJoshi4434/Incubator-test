baac8a66dc2c9d9c5b6a23eb23f0e9bc7e9e66f5
Jeevan Joshi <jeetbhaiya25@gmail.com>
2022-11-08 18:25:16 +0530

    aboutUs bug fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './css/home.css';
 const Footer = () => {
+
   return (
     <div>
       <div className='footer'>
                 <div className='left'>
-                    <Link className='d-flex align-items-center my-2' to="/aboutus"><p>About Us</p></Link>
+                    <p><a className='' href="/aboutus">About Us</a></p>
                     <p><Link to="/contact">Contact Us</Link></p>
                     <p>Privacy Policy</p>
                 </div>
